c4a1ef3107ecf0f986ab8e7a9ac5ca061125bfc6
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-20 14:21:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ function destructivelyRemoveLastDriver() {
 function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
+function appendDriver(name) {
+  return drivers.splice(drivers.length-1, 0, name);
+}
